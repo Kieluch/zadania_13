c600c31a1973c9b6e02846145d0c856ae7f2c3a7
piwkomozna <nwmfilip333@gmail.com>
2024-04-23 14:49:32 +0200

    sortowanie

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,5 +7,10 @@
         tablice.Powitanie();
 
         int[] tab = tablice.Stworz_Tablice();
+
+        Console.Clear();
+        tablice.Wyswietl_Tablice(tab);
+        tablice.Sortowanie_Babelkowe(tab);
+        tablice.Wyswietl_Tablice(tab);
     }
 }
--- ConsoleApp2/tablice.cs
+++ ConsoleApp2/tablice.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 public class Tablice
 {
@@ -11,20 +12,39 @@ public class Tablice
 	public int[] Stworz_Tablice()
 	{		
 		Console.Write("Podaj długość tablicy: ")
-		int dlugosc = int.Parse(Console.ReadLine());
-        int[] tablica = new int[dlugosc];
-		for (int i = 0; i < dlugosc; i++)
+		int dl = int.Parse(Console.ReadLine());
+        int[] tab = new int[dl];
+		for (int i = 0; i < dl; i++)
 		{
 			Console.WriteLine("Podaj kolejny element tablicy: ");
-			tablica[i] = int.Parse(Console.ReadLine());
+			tab[i] = int.Parse(Console.ReadLine());
 		}
 		Console.WriteLine();
-		return tablica;
+		return tab;
     }
 
-	public void Wyswietl_Tablice(int[] tablica)
+	public void Wyswietl_Tablice(int[] tab)
 	{
-		Console.WriteLine(tablica[i]);
+		Console.WriteLine(tab[i]);
 		Console.WriteLine();
 	}
+
+    public void Sortowanie_Babelkowe(int[] tab)
+    {
+        for (int i = 0; i < tab.Length; i++)
+        {
+            for (int j = 0; j < tab.Length - 1; j++)
+            {
+                for (tab[j - 1] = tab[j];)
+                {
+                    int temp = tab[j - 1];
+                    tab[j - 1] = tab[i];
+                    tab[j] = temp;
+                }
+            }
+        }
+    }
+
 }
+
+
