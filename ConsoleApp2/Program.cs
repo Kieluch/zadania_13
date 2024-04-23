71314f6aa65184a5381855f59a8b844355dc13e4
piwkomozna <nwmfilip333@gmail.com>
2024-04-23 13:57:40 +0200

    tworzenie_tablic

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Tablice tablice = new Tablice();
+
+        tablice.Powitanie();
+
+        int[] tab = tablice.Stworz_Tablice();
+    }
+}
---
+++ ConsoleApp2/tablice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Tablice
+{
+	public int = 20
+	public void Powitanie()
+	{
+		Console.WriteLine("Witaj w pliku tablice!");
+	}
+
+	public int[] Stworz_Tablice()
+	{		
+		Console.Write("Podaj długość tablicy: ")
+		int dlugosc = int.Parse(Console.ReadLine());
+        int[] tablica = new int[dlugosc];
+		for (int i = 0; i < dlugosc; i++)
+		{
+			Console.WriteLine("Podaj kolejny element tablicy: ");
+			tablica[i] = int.Parse(Console.ReadLine());
+		}
+		Console.WriteLine();
+		return tablica;
+    }
+}
